4c7b23701c30f2b6473e5c27b1646f0a4e14d6ba
Soumya Khatri <soumya91rias@gmail.com>
2025-11-13 22:41:25 +0530

    Small Edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,7 @@ export default function Home() {
   return (
     <div className="max-w-7xl mx-auto grid grid-rows-[auto_1fr_auto] items-center min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <header className="flex flex-col items-center mb-12">
-        <h1 className="text-4xl font-bold mb-4">React Query Tutorial</h1>
-        <p className="text-lg text-center max-w-3xl">
-          Learn how to use TanStack React Query effectively with this tutorial
-          by{" "}
-          <a
-            href="https://www.youtube.com/c/codinginflow?sub_confirmation=1"
-            className="text-[#4daa57]"
-            target="_blank"
-          >
-            Coding in Flow
-          </a>
-        </p>
+        <h1 className="text-4xl font-bold mb-4">Tan-Stack-React Query</h1>
       </header>
 
       <main className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
